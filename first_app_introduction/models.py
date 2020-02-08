f190a68307a06c996c8ba83ce0f47fff3afd9edc
Xia Xiong <rainbow2019cs@gmail.com>
2020-02-07 20:29:16 -0800

    first_image

--- first_app_introduction/models.py
+++ first_app_introduction/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
 
 # Create your models here.
+class Post(models.Model):
+  title = models.TextField()
+  image = ProcessedImageField(
+    upload_to = 'static/image/posts',
+    format = 'JPEG',
+
+  )
+
+  def __str__(self):
+    return self.title
