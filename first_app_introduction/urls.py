2a14236c7dd41d998d715c54a94cebd1fdcd80e7
Xia Xiong <rainbow2019cs@gmail.com>
2020-02-08 21:56:43 -0800

    post master/detail, creat/update/delete

--- first_app_introduction/urls.py
+++ first_app_introduction/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.HelloDjango.as_view())
+  path("", views.HelloDjango.as_view(),name = "hello_xiongbaobao"),
+  path("posts/",views.PostsView.as_view(), name="posts"),
+  path('post/<int:pk>/', views.PostDetailView.as_view(), name ="post_detail"),
+  path('post/new/',views.PostCreateView.as_view(), name = 'make_post'),
+  path('post/update/<int:pk>/', views.PostUpdateView.as_view(), name ="post_update"),
+  path('post/delete/<int:pk>/', views.PostDeleteView.as_view(), name ="post_delete"),
 ]
